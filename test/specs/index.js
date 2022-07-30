46755933fe4ac8cf9722aab191f106898cf090c8
Pavel Abramuk <pavel.abramuk5@gmail.com>
2022-07-30 12:22:19 +0300

    optimized resources exporting

--- test/specs/index.js
+++ test/specs/index.js
@@ -1,7 +1,6 @@
-const creds = require("../data/credentials.js");
-const testData = require("../data/testData.js");
-const loginPage = require("../pages/login_page");
-const securePage = require("../pages/secure_page.js");
+const loginPage = require("../pages/login.page");
+const securePage = require("../pages/secure.page.js");
+const resources = require("../resources/index.js");
 
 describe("Email test example", () => {
   before(async () => {
@@ -16,20 +15,20 @@ describe("Email test example", () => {
 
   it("Should login", async () => {
     await loginPage.open();
-    await loginPage.login(creds.username, creds.password);
-    await securePage.verifyTitle(`Inbox | ${creds.myEmail} | Proton Mail`);
+    await loginPage.login(resources.username, resources.password);
+    await securePage.verifyTitle(`Inbox | ${resources.myEmail} | Proton Mail`);
   });
 
   it("Should create a new letter", async () => {
     await securePage.openNewMessage();
     await securePage.fillLetter(
-      testData.destination,
-      testData.subject,
-      testData.text
+      resources.destination,
+      resources.subject,
+      resources.letterText
     );
     await securePage.verifyLetterFilledProperly(
-      testData.destination,
-      testData.subject
+      resources.destination,
+      resources.subject
     );
   });
 
@@ -40,14 +39,14 @@ describe("Email test example", () => {
   it("The letter should be present in Draft folder", async () => {
     await securePage.closeLetter();
     await securePage.openDraftsFolder();
-    await securePage.verifyLetterInDrafts(testData.subject);
+    await securePage.verifyLetterInDrafts(resources.subject);
   });
 
   it("The letter contains the same info", async () => {
     await securePage.openLetterFromDrafts();
     await securePage.letterContainsCorrectInfo(
-      testData.destination,
-      testData.subject
+      resources.destination,
+      resources.subject
     );
   });
 
@@ -63,7 +62,7 @@ describe("Email test example", () => {
 
   it("The letter should be present in Sent folder", async () => {
     await securePage.openSentFolder();
-    await securePage.verifyLettersInSent(testData.subject);
+    await securePage.verifyLettersInSent(resources.subject);
   });
 
   it("Delete all letters from Sent folder", async () => {
