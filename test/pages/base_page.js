d20fd4a53e8afe1d6cc2e21d8a938a7a7667e2d1
Pavel Abramuk <pavel.abramuk5@gmail.com>
2022-07-30 12:21:11 +0300

    renamed

--- test/pages/base_page.js
+++
@@ -1,9 +0,0 @@
-module.exports = class BasePage {
-  async open(path) {
-    return await browser.url(path);
-  }
-
-  async verifyTitle(title) {
-    return await expect(browser).toHaveTitle(title);
-  }
-};
