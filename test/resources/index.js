ecd8269129c637042097d9961ebb7989ea23c9af
Pavel Abramuk <pavel.abramuk5@gmail.com>
2022-07-30 12:21:34 +0300

    optimized resources

---
+++ test/resources/index.js
@@ -0,0 +1,15 @@
+const username = "testinaccountforjspurposes";
+const password = "SuperSecretPassword!";
+const myEmail = `${username}@protonmail.com`;
+const destination = "testinaccountforjspurposes@protonmail.com";
+const subject = "Test letter";
+const letterText = "Text example";
+
+module.exports = {
+  username,
+  password,
+  myEmail,
+  destination,
+  subject,
+  letterText,
+};
